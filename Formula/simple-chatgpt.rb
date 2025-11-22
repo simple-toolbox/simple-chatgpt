a09ce7ca77decdb2d1539be76eef9512873c1e31
Paolo Fulgoni <paolo@kota.io>
2025-11-22 19:01:01 +0100

    Moved to simple-toolbox org

--- Formula/simple-chatgpt.rb
+++ Formula/simple-chatgpt.rb
@@ -1,11 +1,11 @@
 class SimpleChatgpt < Formula
   desc "Simple command-line tool to use ChatGPT Desktop from your Mac terminal"
-  homepage "https://github.com/paolofulgoni/simple-chatgpt"
+  homepage "https://github.com/simple-toolbox/simple-chatgpt"
   license "MIT"
-  url "https://github.com/paolofulgoni/simple-chatgpt/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "2e0770c9cbc76cb88bd2d0f67bc04bbb839e1c54a7ee2c036f980c1974499d4a"
+  url "https://github.com/simple-toolbox/simple-chatgpt/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "c2094611d376fd61f3a52438a49d62fe55e71d6823c6b9ed842c226a81b8ad9a"
   version "0.2.0"
-  head "https://github.com/paolofulgoni/simple-chatgpt.git", branch: "main"
+  head "https://github.com/simple-toolbox/simple-chatgpt.git", branch: "main"
 
   def install
     bin.install "simple-chatgpt"
